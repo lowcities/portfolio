441089430faf63fb9af032c6203a07682eb4f9a1
David Low <musumashae@gmail.com>
2017-03-22 16:38:39 -0400

    Added files to repository

---
+++ js/.js-live-code.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(function(){
+		$('#menu').slicknav({
+            label: '',
+            appendTo: '.append',
+            brand: "Site Navigation"
+        });
+
+
+    });
+
+
+});
+
+var x = 3;
+console.log(x)
+
+if(x >= 5){
+    var x += 1;
+}
